e2ec490106344c002e45250a3d442655f2767a9d
9534495797 <chaubeyankit927@gmail.com>
2023-03-20 11:08:26 +0530

    ok

---
+++ day1.py
@@ -0,0 +1,14 @@
+# print("hello world")
+# x=int(input("value= "))
+# print(x)
+# x=float(input("value= "))
+# print(x)
+# x=str(input("value= "))
+# print(x)
+
+
+# x=int(input("value= "))
+# y=int(input("value= "))
+# print("sum= diff= multi= divide=",x+y,x-y,x*y,x/y)
+
+
