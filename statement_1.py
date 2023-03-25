328e17d5ccf5468462d2c88b1279dd1390de9613
9534495797 <chaubeyankit927@gmail.com>
2023-03-25 22:05:48 +0530

    'ok'

---
+++ statement_1.py
@@ -0,0 +1,117 @@
+# x=int(input())
+# if x>90:
+#     print("grade-A")
+# elif x>80 and x<=90:
+#     print("grade-B")
+# elif x>=60 and x<=80:
+#     print("grade-C")
+# else:
+#     print("grade-D")
+
+#question 2
+# x=int(input())
+# if x>100000:
+#     print("Tax-15%")
+# elif x>50000 and x<=100000:
+#     print("TAX-10%")
+# else:
+#     print("Tax-5%")
+
+
+#question3
+# x=int(input())
+# if x==1:
+#    print("month-january days-31")
+# elif x==2:
+#     print("february-28")
+# elif x==3:
+#     print("march-31") 
+# elif x==4:
+#     print("aprail-30")
+# elif x==5:
+#     print("may-31") 
+# elif x==6:
+#     print("june-30")
+# elif x==7:
+#     print("july-31") 
+# elif x==8:
+#     print("august-31")
+# elif x==9:
+#     print("september-30") 
+# elif x==10:
+#     print("october-31")
+# elif x==11:
+#     print("november-30") 
+# else:
+#     print("december-31")
+
+#question 4
+# x=input("select city(Delhi,Agra,Jaipur): ")
+# if x=="Delhi":
+#     print("Redfort")
+# elif x=="Agra":
+#     print("Taj mahal")
+# elif x=="Jaipur":
+#     print("Jal mahal")
+
+#question 5
+# x=int(input())
+# if x>5 and x<=10:
+#     print("Hello")
+# else:
+#     print("Bye")
+
+#question 6
+# x=int(input())
+# if x>=100:
+#     print("yes 3 digit")
+# else:
+#     print("no")
+
+# question 7
+# x=int(input())
+# if x%2==0:
+#     print("Even")
+# else:
+#     print("odd")
+
+
+
+# question 8
+# x=int(input())
+# if x>=0:
+#     print("positive")
+# else:
+#     print("negative")
+
+#question 9
+# x=int(input())
+# if x%2==0 and x%3==0:
+#     print("yes")
+# else:
+#     print("no")
+#question 10
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())
+# if a<b and a<c and a<d:
+#     print("a youngest")
+# elif b<a and b<c and b<d:
+#     print("b youngest")
+# else:
+#     print("c youngest")
+
+# question 11
+# x=int(input())
+# if x%1==0 and x%x==0:
+#     print("Prime")
+# else:
+#     print("not")
+
+#quuestion 12
+x=input('')
+if x=='a' or x=='e' or x=='i' or x=='o' or x=='u' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U': 
+    print("vowels")
+else:
+    print("constants")
