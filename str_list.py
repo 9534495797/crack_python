2a59452effd7426d9e86cb16bd64ebdb191def8d
9534495797 <chaubeyankit927@gmail.com>
2023-03-26 19:46:32 +0530

    'ok'

---
+++ str_list.py
@@ -0,0 +1,65 @@
+#palindrome
+# x=input("")
+# if x==x[::-1]:
+#     print("yes palindrome")
+# else:
+#     print("no")
+
+# def palin(x):
+#     return x[::-1]
+# x=input()
+# if x==x[::-1]:
+#     print("yes")
+# else:
+#     print("no")
+
+
+#find duplicacy
+# x="apple"
+# first=[]
+# dup=[]
+# for i in x:
+#     if i not in first:
+#         first.append(i)
+#     else:
+#             if i not in dup:
+#                  dup.append(i)
+#                  print("".join(dup))
+            
+
+#transpose
+# a=[[1,2],
+#    [3,4],    
+#    [5,6]]
+# trans=[[0,0,0],
+#        [0,0,0]
+# ]
+# for i in range(len(a)):
+#     for j in range(len(a[0])):
+#         trans[j][i]=a[i][j]
+#     for k in trans:
+#         print (k)
+
+
+#add matrix
+# a=[[1,2,3],
+#    [2,3,4],
+#    [3,4,5]]
+# b=[[1,2,3],
+#    [2,3,4],
+#    [3,4,5]]
+# c=[[0,0,0],
+#    [0,0,0],
+#    [0,0,0]]
+# for i in range(len(a)):
+#     for j in range(len(a[0])):
+#           c[i][j]=a[i][j]+b[i][j]
+#     for k in c:
+#         print(k)
+
+
+
+
+
+        
+    
