92afc136bbdddf18bd005dd250451c049d8a0259
9534495797 <chaubeyankit927@gmail.com>
2023-03-26 19:47:00 +0530

    ok

---
+++ tuple_dict.py
@@ -0,0 +1,36 @@
+#cube between 1 and 4 and values are cube of that
+# def dictonary():
+#     n=dict()
+#     n[1]==1**3
+#     n[2]==2**3
+#     n[3]==3**3
+#     n[4]==4**3
+
+#     print (n)
+# dictonary()
+
+
+# two part question
+# tup=(1,2,3,4,5,6,7,8,9,10)
+# tup1=tup[:5]
+# tup2=tup[5:11]
+# print(tup1)
+# print(tup2)
+
+
+#find even in a tuple
+# x=(1,2,3,4,5,6,7,8)
+# y=list()
+# for i in x:
+#     if i%2==0:
+#         y.append(i)
+#     print(tuple(y))
+
+
+x=int(input()).split('')
+for i in range(len(x)):
+    i+=1
+
+
+    
+
