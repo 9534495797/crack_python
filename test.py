4568a29a5d39975360aa6f314378a75361767af6
9534495797 <chaubeyankit927@gmail.com>
2023-04-02 19:00:50 +0530

    ok

---
+++ test.py
@@ -0,0 +1,33 @@
+# def sayhello(username):
+#     greet="hello"
+#     print(greet+","+username)
+
+
+# l=['ankit','ram','rahul']
+# for name in l:
+#     sayhello(name)
+
+
+
+# def gcd(a,b):
+#     if b==0:
+#         return a
+#     else:
+#         return gcd(b,a%b)
+# num1=int(input())
+# num2=int(input())
+# print(gcd(num1,num2))
+
+# class person():
+#     def setname(self,name):
+#         self.name=name
+#     def getname(self,name):
+#         self.name=name
+# p1=person()
+# p2=person()
+# name1=input("")
+# p1.setname(name1)
+# name2=input("")
+# p2.setname(name2)
+# print(p1.getname())
+# print(p2.getname())
