7ff9696af4957a33ac822ca0300939a6fa2d2cce
9534495797 <chaubeyankit927@gmail.com>
2023-04-02 19:00:21 +0530

    ok

---
+++ transpose.py
@@ -0,0 +1,13 @@
+matrix=[[1,2,3],
+        [2,3,4],
+        [3,4,5],
+        [4,5,6]]
+trans=[[0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]
+        ]
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        trans[j][i]=matrix[i][j]
+for k in trans:
+    print(k)
