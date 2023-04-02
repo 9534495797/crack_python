3745681ab16ea97f1b65d63056bb4a8f81916853
9534495797 <chaubeyankit927@gmail.com>
2023-04-02 19:01:26 +0530

    ok

---
+++ area.py
@@ -0,0 +1,16 @@
+# def area(a,b):
+#     return 1/2*(a*b)
+# a=int(input())
+# b=int(input())
+# print(area(a,b))
+# import math
+# class cal():
+#     def area(self,a):
+#         return math.pi*a**2
+# n=cal()
+# a=int(input())
+# print(n.area(a))
+import math
+def area(dia):
+    return math.pi*dia**2
+print(area(1))
