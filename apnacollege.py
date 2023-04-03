d59d26790ce6003f10be9a51b66753f50a7fa1b3
9534495797 <chaubeyankit927@gmail.com>
2023-04-03 18:49:21 +0530

    ok

--- apnacollege.py
+++ apnacollege.py
@@ -1,1 +1,95 @@
-print("HELLO WORLD!")
+# print("HELLO WORLD!")
+# name="ankit"
+# age=21
+# print(name,age)
+
+
+
+# input("what is your name")
+# num=100
+# print(bool(num))
+x="ankit"
+# print(x.upper())
+print(x.find("a"))
+# print(x.replace("ankit","satya"))
+# print("a" in x)
+print("f" in x)
+
+
+
+
+
+
+# print(4+5,4-5,4*5,4/5,4//5,4%5,4**2)
+
+
+# print(2+3*5)
+# print(((2+3))*5)
+
+# print(3>2)
+# print(3<2)
+# print(3==3)
+# print(3!=3)
+
+
+# print(not 2>3)
+
+#x=int(input())
+# operator=input()
+# y=int(input())
+# if operator=="+":
+#     print(x+y)
+# elif operator=="-":
+#     print(x-y)
+# elif operator=="*":
+#     print(x*y)
+# elif operator=="/":
+#     print(x/y)
+# elif operator=="%":
+#     print(x%y)
+# else:
+#     print("invalid")
+
+# for i in range(1,100):
+#     print(i)
+
+
+
+# i=1
+# while i<=5:
+#     print(i*"*")
+#     i+=1
+
+# i=5
+# while i>=0:
+#     print(i*"*")
+#     i-=1
+
+
+# for i in range(5):
+#     print(i)
+# for i in range(5):
+#     print(i+4)
+
+# x=[1,2,3,4,5]
+# print(5 in x)
+# print(10 in x)
+# x=[1,2,3,4,5]
+# x.clear()
+# print(x)
+
+# x=['a','b','c','d']
+# for i in x:
+#     if i=="c":
+#         break
+#     print(i)
+
+# x=['a','b','c','d']
+# for i in x:
+#     if i=="c":
+#         continue
+#     print(i)
+
+x="ankit"
+
+print(list(x),end='')
