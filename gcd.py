d1c32b4dee8073fe7a06202ea4d06ffe2146f975
9534495797 <chaubeyankit927@gmail.com>
2023-04-03 18:50:45 +0530

    ok'

---
+++ gcd.py
@@ -0,0 +1,28 @@
+# # Get user input
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# # # Define function to find gcd using Euclidean algorithm
+# def gcd(num1, num2):
+#     if num2 == 0:
+#         return num1
+#     else:
+#         return gcd(num2, num1 % num2)
+
+# # Call the function and print the result
+# result = gcd(num1, num2)
+# print("The GCD of", num1, "and", num2, "is:", result)
+
+# def gcd(a,b):
+#     if b==0:
+#         return a
+#     else:
+#         return gcd(b,a%b)
+# print(gcd(4,8))
+
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+print(gcd(3,4))
