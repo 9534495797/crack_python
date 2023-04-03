a68791250459ea88eee4202c031a112855aa9cc6
9534495797 <chaubeyankit927@gmail.com>
2023-04-03 18:50:20 +0530

    ok

--- allinone.py
+++ allinone.py
@@ -71,3 +71,47 @@
 # while i<=num*10:
 #     print(i)
 #     i+=num
+
+
+class a():
+    def factorial(self,num):
+        if num==0 or num==1:
+            return 1
+        else:
+            return num *self. factorial(num-1)
+    def fibo(self,num):
+        if num==0:
+            return 0
+        elif num==1:
+            return 1
+        else:
+            return self.fibo(num-1)+self.fibo(num-2)
+    def factor(self,num):
+        for i in range(1,num+1):
+            if num%i==0:
+                print(i)
+    def armstrong(self,num):
+        n=len(str(num))
+        sum=0
+        for char in str(num):
+            sum+=int(char)**n
+            if num==sum:
+                return True
+            else:
+                return False
+            
+    def sum(self,num):
+        if num==0:
+            return 0
+        elif num==1:
+            return 1
+        else:
+            return num+self.sum(num-1)
+b=a()
+num=int(input())
+print(b.factorial(num))
+for i in range(num+1):
+    print("fibo",b.fibo(i))
+print("factor",b.factor(num))
+print("arm",b.armstrong(num))
+print("sum",b.sum(num))
