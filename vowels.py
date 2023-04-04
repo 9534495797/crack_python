101f5dfc7e5aedd33bebdfa122fad3c03799208e
9534495797 <chaubeyankit927@gmail.com>
2023-04-04 18:49:55 +0530

    ok

---
+++ vowels.py
@@ -0,0 +1,48 @@
+# def count_vowels(string):
+#     count = 0
+#     vowels = 'aeiouAEIOU'
+#     for char in string:
+#         if char in vowels:
+#             count += 1
+#     return count
+# print(count_vowels("education"))
+
+# def counting(string):
+#     count=0
+#     vowels="aeiouAEIOU"
+#     for char in string:
+#         if char in vowels:
+#             count+=1
+#     return count
+# print(counting("AaNnkIIT"))
+
+# def count_vowels(string):
+#     vowelcount=0
+    
+#     vowels="aeiouAEIOU"
+#     for char in string:
+#         if char in vowels:
+#             vowelcount+=1
+#     return vowelcount
+
+# def count_vowels(string):
+#     count=0
+#     vowels="aeiouAEIOU"
+#     for char in string:
+#         if char in vowels:
+#             count+=1
+#     return count
+# string=input("")
+# print(count_vowels(string))
+
+    
+# print(count_vowels("education"))
+
+def  v0wel_count(string):
+    count=0
+    vowels="aeiouAEIOU"
+    for char in string:
+        if char in vowels:
+            count+=1
+    return count
+print(v0wel_count("education"))
