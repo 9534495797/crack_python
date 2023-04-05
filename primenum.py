6e3765d50da90984f5be76f55a573944b93f160c
9534495797 <chaubeyankit927@gmail.com>
2023-04-05 21:11:07 +0530

    ok

---
+++ primenum.py
@@ -0,0 +1,36 @@
+# #prime number
+# a=int(input())
+# b=int(input())
+# for i in range(a,b+1):
+#     if i>1:
+#         for j in range(2,i):
+#             if i%j==0:
+#                 break
+#             else:
+#                 print(i)
+#                 break
+
+
+# def prime(num):
+#     for i in range(num+1):
+#         if i>1:
+#             for j in range(2,i):
+#                 if i%j==0:
+#                     break
+#                 else:
+#                     print(i)
+#                     break
+# num=int(input())
+# print(prime(num))
+
+def prime(num):
+    for i in range(num+1):
+        if i>1:
+            for j in range(2,i):
+                if i%j==0:
+                    break
+                else:
+                    print(i)
+                    break
+print(prime(10))
+
