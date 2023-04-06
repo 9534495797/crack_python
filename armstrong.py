d6668015a432fb025788ac67720f75e34cd4741d
9534495797 <chaubeyankit927@gmail.com>
2023-04-06 18:45:32 +0530

    ok

---
+++ armstrong.py
@@ -0,0 +1,63 @@
+# def is_armstrong(num):
+#     # Count the number of digits
+#     n = len(str(num))
+#     sum = 0
+#     for digit in str(num):
+#         sum += int(digit)**n
+#     if sum == num:
+#         return True
+#     else:
+#         return False
+# num=int(input())
+# print(is_armstrong(num))
+
+    
+# class a():
+#     def armstrong(num):
+#         n=len(str(num))
+#         sum=0
+#         for char in str(num):
+#             sum+=int(char)**n
+#             if sum==num:
+#                 return True
+#             else:
+#                 return False
+# b=a()
+# num=int(input())
+# print(a.armstrong(num))
+
+# def armstrong(num):
+#     b=len(str(num))
+#     sum=0
+#     for char in str(num):
+#         sum+=int(char)**b
+#         if sum==num:
+#             return True
+#         else:
+#             return False
+# num=int(input())
+# print(armstrong(num))
+
+# def armstrong(num):
+    
+#     b=len(str(num))
+#     sum=0
+#     for char in str(num):
+#         sum+=int(char)**b
+#         if sum==num:
+#             return True
+#         else:
+#             return False
+# num=int(input())
+# print(armstrong(num))
+
+def armstrong(num):
+    sum=0
+    b=len(str(num))
+    for i in str(num):
+        sum+=int(i)**b
+        if sum==num:
+            return True
+        else:
+            return False
+print(armstrong(54))
