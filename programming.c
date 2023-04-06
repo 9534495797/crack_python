6882fa63e87683de1ed64f14d46f9a0385fda0ee
9534495797 <chaubeyankit927@gmail.com>
2023-04-06 18:46:45 +0530

    ok

---
+++ programming.c
@@ -0,0 +1,26 @@
+// #include<stdio.h>
+// int main(){
+//     printf("hello world");
+// }
+
+// What is the difference between an int and a float data type in C?
+
+// How do you declare a variable in C?
+
+// What is a function in C?
+
+// What is the purpose of the main function in a C program?
+
+// How do you print something to the console in C?
+
+// What is a conditional statement in C? Give an example.
+
+// How do you declare and use an array in C?
+
+// What is a loop in C? Give an example of a loop.
+
+// How do you read input from the user in C?
+
+// How do you write to a file in C?
+
+// These are just a few basic questions to get you started. If you have any specific questions or areas you would like to focus on, please let me know.
