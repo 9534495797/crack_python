f3c8586ccecc6aa241c712d88b766f25b1ae4bc0
9534495797 <chaubeyankit927@gmail.com>
2023-04-07 07:17:23 +0530

    ok'

---
+++ tupleallinone.py
@@ -0,0 +1,11 @@
+# tup1=(1,2,3,4)
+# tup2=(4,5,6,7)
+# c=zip(tup1,tup2)
+# print(c)
+# tup=(1,2,3,4)
+# print(len(tup))
+
+# print(max(tup))
+# print(min(tup))
+
+#tuple is immmutable so we doa all 
