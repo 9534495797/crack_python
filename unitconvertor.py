55b48320ff18a732fc0533584a59feb7a8ddbaf0
9534495797 <chaubeyankit927@gmail.com>
2023-04-09 19:31:41 +0530

    ok

---
+++ unitconvertor.py
@@ -0,0 +1,35 @@
+# def convert(c): c to kelvin
+#     return 273+c
+# c=int(input())
+# print(convert(c))
+ 
+# def convert(f):  f to inch
+#     return f*12
+# print(convert(4))
+
+# def convert(f):    f to c
+#     return ((f-32)/180)*100
+# print(convert(77))
+
+
+# class temp():
+#     def convert(self,f):    #f to c
+#      return ((f-32)/180)*100
+# n=temp()
+# f=int(input())
+# print(n.convert(f))
+
+# def convert(p):
+#     return p*0.453  pound to kg
+# print(convert(4))
+
+
+# def convert(m):
+#     return m*60
+# print(convert(60))
+
+
+def convert(f):
+    return ((f-32)/180)*100
+
+print(convert(32))
